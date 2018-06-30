7b6927fb4c18a79d8b916b9561ae834f5c789046
Timdows <tim@inexpro.nl>
2018-06-30 12:39:14 +0200

    Move get1400 command

---
+++ TimelapseMP4.Creator/Commands/Get1400HourFilesCommand.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Transforms;
+using TimelapseMP4.Creator.Models;
+
+namespace TimelapseMP4.Creator.Commands
+{
+	public static class Get1400HourFilesCommand
+	{
+		public static void Get1400HourFiles()
+		{
+			var saveDir = "1400HourFiles";
+			if (!Directory.Exists(saveDir))
+			{
+				Directory.CreateDirectory(saveDir);
+			}
+
+			var fileGroup = Directory.GetFiles(@"\\192.168.1.14\projects\VWP Timelapse\timelapse construction", "*.jpg", SearchOption.AllDirectories)
+				.Select(item => ImageFileDetails.CreateImageFileDetails(item))
+				.Where(item => item != null)
+				.GroupBy(item => item.DateTimeTaken.Date)
+				.ToList();
+
+			foreach (var files in fileGroup)
+			{
+				var fileAfter = files
+					.OrderBy(item => item.DateTimeTaken)
+					.FirstOrDefault(item => item.DateTimeTaken.Hour >= 14);
+
+				var fileBefore = files
+					.OrderByDescending(item => item.DateTimeTaken)
+					.FirstOrDefault(item => item.DateTimeTaken.Hour < 14);
+
+				ImageFileDetails saveFile = null;
+				if ((fileAfter?.DateTimeTaken.Hour ?? 23) - 14 <= 14 - (fileBefore?.DateTimeTaken.Hour ?? 0))
+				{
+					saveFile = fileAfter;
+				}
+
+				if ((fileAfter?.DateTimeTaken.Hour ?? 23) - 14 >= 14 - (fileBefore?.DateTimeTaken.Hour ?? 0))
+				{
+					saveFile = fileBefore;
+				}
+
+				if (saveFile == null && fileBefore != null)
+				{
+					saveFile = fileBefore;
+				}
+
+				if (saveFile == null)
+				{
+					continue;
+				}
+				else
+				{
+					try
+					{
+						// Load the image and save a resized version
+						using (var image = Image.Load(saveFile.Path))
+						{
+							image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+							image.Save($"{saveDir}\\{saveFile.DateTimeTaken.ToString("yyyy-MM-ddTHHmmss")}.jpg");
+						}
+
+						// Load the image and save a thumbnail
+						using (var image = Image.Load(saveFile.Path))
+						{
+							var height = 200;
+							decimal widthRatio = image.Height / height;
+							int width = (int)Math.Round(image.Width / widthRatio, 0);
+							image.Mutate(x => x.Resize(width, height));
+							image.Save($"{saveDir}\\{saveFile.DateTimeTaken.ToString("yyyy-MM-ddTHHmmss")}_thumb.jpg");
+						}
+					}
+					catch (Exception excep)
+					{
+						var a = excep.Message;
+					}
+					
+				}
+			}
+		}
+	}
+}
--- TimelapseMP4.Creator/Program.cs
+++ TimelapseMP4.Creator/Program.cs
@@ -13,9 +13,7 @@ using TimelapseMP4.Creator.Models;
 namespace TimelapseMP4.Creator
 {
 	public class Program
-	{
-		
-
+	{	
 		public static async Task Main(string[] args)
 		{
 			//Get1400HourFiles();
@@ -55,9 +53,7 @@ namespace TimelapseMP4.Creator
 				await GetFilesAndSaveResizedCommand.GetFilesAndSaveResized(sourceDirectory, destinationDirectory);
 				//await CreateTimelapseMP4(appSettings, destinationDirectory, date);
 			}
-		}
-
-		
+		}		
 
 		public static async Task CreateTimelapseMP4(AppSettings appSettings, string localImageDirectory, string filename)
 		{
@@ -130,80 +126,6 @@ namespace TimelapseMP4.Creator
 			Console.WriteLine($"Finished creating movie in {elapsedMillis} ms");
 			result += $"\r\nelapsedMillis: {elapsedMillis}\r\n";
 			await LogCreateOutput(result, filename);
-		}
-
-		public static void Get1400HourFiles()
-		{
-			var saveDir = "1400HourFiles";
-			if (!Directory.Exists(saveDir))
-			{
-				Directory.CreateDirectory(saveDir);
-			}
-
-			var fileGroup = Directory.GetFiles(@"\\192.168.1.14\projects\VWP Timelapse\timelapse construction", "*.jpg", SearchOption.AllDirectories)
-				.Select(item => ImageFileDetails.CreateImageFileDetails(item))
-				.Where(item => item != null)
-				.GroupBy(item => item.DateTimeTaken.Date)
-				.ToList();
-
-			foreach (var files in fileGroup)
-			{
-				var fileAfter = files
-					.OrderBy(item => item.DateTimeTaken)
-					.FirstOrDefault(item => item.DateTimeTaken.Hour >= 14);
-
-				var fileBefore = files
-					.OrderByDescending(item => item.DateTimeTaken)
-					.FirstOrDefault(item => item.DateTimeTaken.Hour < 14);
-
-				ImageFileDetails saveFile = null;
-				if ((fileAfter?.DateTimeTaken.Hour ?? 23) - 14 <= 14 - (fileBefore?.DateTimeTaken.Hour ?? 0))
-				{
-					saveFile = fileAfter;
-				}
-
-				if ((fileAfter?.DateTimeTaken.Hour ?? 23) - 14 >= 14 - (fileBefore?.DateTimeTaken.Hour ?? 0))
-				{
-					saveFile = fileBefore;
-				}
-
-				if (saveFile == null && fileBefore != null)
-				{
-					saveFile = fileBefore;
-				}
-
-				if (saveFile == null)
-				{
-					continue;
-				}
-				else
-				{
-					try
-					{
-						// Load the image and save a resized version
-						using (var image = Image.Load(saveFile.Path))
-						{
-							image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
-							image.Save($"{saveDir}\\{saveFile.DateTimeTaken.ToString("yyyy-MM-ddTHHmmss")}.jpg");
-						}
-
-						// Load the image and save a thumbnail
-						using (var image = Image.Load(saveFile.Path))
-						{
-							var height = 200;
-							decimal widthRatio = image.Height / height;
-							int width = (int)Math.Round(image.Width / widthRatio, 0);
-							image.Mutate(x => x.Resize(width, height));
-							image.Save($"{saveDir}\\{saveFile.DateTimeTaken.ToString("yyyy-MM-ddTHHmmss")}_thumb.jpg");
-						}
-					}
-					catch (Exception excep)
-					{
-						var a = excep.Message;
-					}
-					
-				}
-			}
 		}
 
 		public static void UnsortedFiles(AppSettings appSettings)
