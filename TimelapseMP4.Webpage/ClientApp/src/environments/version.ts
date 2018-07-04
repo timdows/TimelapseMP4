89a58da779a5d38eb10b72e7b94b21aecf2f2e50
Timdows <tim@inexpro.nl>
2018-07-04 11:26:51 +0200

    Loading styles

--- TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
+++ TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = '239a8ec';
+export const gitShort = 'f8db305';
 
-export const gitLong = '239a8ec9c4067bb70885174f33b7c12ce7bdb14e';
+export const gitLong = 'f8db30539c87001b2348136d8f9915dab6cd78f1';
 
-export const gitDate = 'Wed Jul 04 2018 09:07:33 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Wed Jul 04 2018 11:14:57 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
