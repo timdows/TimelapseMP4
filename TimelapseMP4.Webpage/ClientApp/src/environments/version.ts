d05095e848825c04223285000b10b1c3f186e4cf
Timdows <tim@inexpro.nl>
2018-07-04 13:21:21 +0200

    Hour 1400 changes

--- TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
+++ TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = 'f8db305';
+export const gitShort = 'fb06ef7';
 
-export const gitLong = 'f8db30539c87001b2348136d8f9915dab6cd78f1';
+export const gitLong = 'fb06ef7dd62314358d667f4ec555a90cee1e5b82';
 
-export const gitDate = 'Wed Jul 04 2018 11:14:57 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Wed Jul 04 2018 13:04:29 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
