f23ab3c2e352496303ba40e2a492ceb61b809e4a
Timdows <tim@inexpro.nl>
2018-07-17 22:22:29 +0200

    Create init command

---
+++ TimelapseMP4.Creator/Commands/InitCommand.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Transforms;
+using TimelapseMP4.Creator.Models;
+
+namespace TimelapseMP4.Creator.Commands
+{
+	public static class InitCommand
+	{
+		public static async Task DoInit(AppSettings appSettings)
+		{
+		}
+	}
+}
--- TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
+++ TimelapseMP4.Webpage/ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = 'fb06ef7';
+export const gitShort = 'd05095e';
 
-export const gitLong = 'fb06ef7dd62314358d667f4ec555a90cee1e5b82';
+export const gitLong = 'd05095e848825c04223285000b10b1c3f186e4cf';
 
-export const gitDate = 'Wed Jul 04 2018 13:04:29 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Wed Jul 04 2018 13:21:21 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
